0d3855cc6159a6ab190ef292c3f0169cb628fce1
Ajeet Gupta <agupta2@illumina.com>
2015-10-28 16:08:19 -0700

    Adding test file for the rest services

---
+++ src/test/java/services/ConceptModelControllerTest.java
@@ -0,0 +1,29 @@
+package services;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by agupta2 on 10/28/15.
+ */
+public class ConceptModelControllerTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testAddConcept() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void testGetConceptById() throws Exception {
+
+    }
+}
